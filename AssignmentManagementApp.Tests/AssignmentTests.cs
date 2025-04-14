e35a85d31ed5a5351a898c60782f355b309b9353
MagShadowLight <williamrose1025@icloud.com>
2025-04-13 20:48:11 -0700

    Add project files.

---
+++ AssignmentManagementApp.Tests/AssignmentTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagementApp.Tests
+{
+    public class AssignmentTests
+    {
+
+        [Fact]
+        public void Assignment_Should_Have_A_Title()
+        {
+            var assignment = new Assignment();
+            Assert.NotNull(assignment.Title);
+        }
+
+    }
+}
---
+++ AssignmentManagementApp/Assignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagementApp
+{
+    public class Assignment
+    {
+        public string Title { get; set; } = "";
+    }
+}
