8ae2da70597a3ac7f69fcae00dae22cf30abc880
MagShadowLight <williamrose1025@icloud.com>
2025-04-22 10:03:48 -0700

    Add AssignmentService Class

---
+++ AssignmentManagementApp/AssignmentService.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagementApp
+{
+    internal class AssignmentService
+    {
+        private readonly List<Assignment> assignments = new();
+
+        public void AddAssignment(Assignment assignment)
+        {
+            if (assignment == null)
+                throw new ArgumentNullException(nameof(assignment));
+
+            assignments.Add(assignment);
+        }
+
+        public List<Assignment> ListAll()
+        {
+            return assignments;
+        }
+    }
+}
